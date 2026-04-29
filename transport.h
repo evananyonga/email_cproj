bd7d648d56ad417d01723f35100a210382096860
evananyonga <evewish@gmail.com>
2026-04-29 15:58:22 -0700

    define transport object structure

---
+++ transport.h
@@ -0,0 +1,14 @@
+#ifdef TRANSPORT_H
+#define TRANSPORT_H
+
+#include "email.h"
+
+typedef struct {
+    const char* name;
+    int (*send)(Email *email);
+    int (*validate)(void);
+} Transport;
+
+Transport *get_transport(const char *name);
+
+#endif // TRANSPORT_H
