307af6930224deee9ea7405a5855a0877db8d8cc
evananyonga <evewish@gmail.com>
2026-05-06 19:26:22 -0700

    allow class usage in dns resolver

---
+++ smtp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include "sendmail.h"
+#include "transport.h"
+
---
+++ smtp.h
@@ -0,0 +1,8 @@
+#ifndef SMTP_H
+#define SMTP_H
+
+#include "transport.h"
+
+extern Transport smtp_transport;
+
+#endif // SMTP_H
