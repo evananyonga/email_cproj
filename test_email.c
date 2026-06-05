efd5adb3eefa3703d506553626e9824f5818459d
evananyonga <evewish@gmail.com>
2026-06-05 08:00:35 -0700

    test email body existence

--- test_email.c
+++ test_email.c
@@ -46,9 +46,26 @@ void test_email_has_subject() {
     TEST_ASSERT(validate_email(&email) == 0, "NULL subject email fails validation");
 }
 
+void test_email_has_body() {
+    Email email;
+    email.from = "email@email.com";
+    email.to = "recipient@email.com";
+    email.subject = "Test Hello";
+    email.body = "Test Body";
+    TEST_ASSERT(validate_email(&email) == 1, "Valid email passes validation");
+
+    email.body = "";
+    TEST_ASSERT(validate_email(&email) == 0, "Empty body email fails validation");
+
+    email.body = NULL;
+    TEST_ASSERT(validate_email(&email) == 0, "NULL body email fails validation");
+}
+
 int main() {
     TEST_RUN(test_from_is_valid);
     TEST_RUN(test_to_is_valid);
+    TEST_RUN(test_email_has_subject);
+    TEST_RUN(test_email_has_body);
     TEST_SUMMARY();
     return tests_failed > 0 ? 1 : 0;
 }
