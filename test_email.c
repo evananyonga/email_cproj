4cac26a1fb5c877923742de29c920a462525c1f2
evananyonga <evewish@gmail.com>
2026-06-07 06:16:12 -0700

    test subject doesnt exceed 50

--- test_email.c
+++ test_email.c
@@ -61,11 +61,30 @@ void test_email_has_body() {
     TEST_ASSERT(validate_email(&email) == 0, "NULL body email fails validation");
 }
 
+void test_subject_max_length() {
+    Email email;
+    email.from = "email@email.com";
+    email.to = "recipient@email.com";
+    email.body = "Test Body";
+    // char long_subject[256];
+    // memset(long_subject, 'A', sizeof(long_subject) - 1);
+    // // long_subject[sizeof(long_subject) - 1] = '\0';
+    // for (int i = 0; i < sizeof(long_subject) - 1; i++) {
+    //     long_subject[i] = 'A';
+    // }
+    email.subject = "This is a long subject that exceeds 50 subject characters.";
+    TEST_ASSERT(validate_email(&email) == 0, "Subject over 50 subject characters fails validation");
+
+    email.subject = "Short subject";
+    TEST_ASSERT(validate_email(&email) == 1, "Valid subject email passes validation");
+}
+
 int main() {
     TEST_RUN(test_from_is_valid);
     TEST_RUN(test_to_is_valid);
     TEST_RUN(test_email_has_subject);
     TEST_RUN(test_email_has_body);
+    TEST_RUN(test_subject_max_length);
     TEST_SUMMARY();
     return tests_failed > 0 ? 1 : 0;
 }
